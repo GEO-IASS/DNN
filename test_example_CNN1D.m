c25c6c06d6f5d3458e3d52fa016bed3c3c81e89d
郑毅 <rt77789@163.com>
2013-04-11 11:44:36 +0800

    1-D time series CNN.

---
+++ test_example_CNN1D.m
@@ -0,0 +1,72 @@
+function test_example_CNN1D(gen_x, gen_y, test_x, test_y)
+
+% gen_x = repmat(sin(0.1:0.1:25.6), 10000, 1)';
+% gen_y = repmat([0 1]', 1, 10000);
+
+%/Users/rt77789/code/R
+
+% tri_matrix = load('/Users/rt77789/code/R/tri_matrix.data');
+% sin_matrix = load('/Users/rt77789/code/R/sin_matrix.data');
+% 
+% [row, col] = size(tri_matrix);
+% 
+% gen_x = [tri_matrix(:, 1:(col-1)); sin_matrix(:, 1:(col-1))]';
+% tri_y = zeros(2, row);
+% sin_y = zeros(2, row);
+% 
+% for i = 1:row
+%     tri_y(tri_matrix(i, col), i) = 1;
+%     sin_y(sin_matrix(i, col), i) = 1;
+% end
+% 
+% gen_y = [tri_y, sin_y];
+% 256 -> 
+
+
+%% ex1 Train a 6c-2s-12c-2s Convolutional neural network 
+%will run 1 epoch in about 200 second and get around 11% error. 
+%With 100 epochs you'll get around 1.2% error
+rng(0)
+cnn.layers = {
+    struct('type', 'i') %input layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 5) %convolution layer
+    struct('type', 's', 'scale', 2) %sub sampling layer
+    struct('type', 'c', 'outputmaps', 16, 'kernelsize', 5) %convolution layer
+    struct('type', 's', 'scale', 2) %subsampling layer
+    
+    %% more layers.
+   % struct('type', 'c', 'outputmaps', 32, 'kernelsize', 4) %convolution layer
+   % struct('type', 's', 'scale', 2) %sub sampling layer
+   % struct('type', 'c', 'outputmaps', 64, 'kernelsize', 4) %convolution layer
+   % struct('type', 's', 'scale', 2) %subsampling layer
+};
+cnn = cnn1Dsetup(cnn, gen_x, gen_y);
+
+opts.alpha = 1;
+opts.batchsize = 50;
+opts.numepochs = 1;
+
+cnn = cnn1Dtrain(cnn, gen_x, gen_y, opts);
+
+[er, bad] = cnn1Dtest(cnn, test_x, test_y);
+disp(['test error=' num2str(er)]);
+
+%plot mean squared error
+figure; 
+%plot(cnn.rL);
+hold on;
+for i = 1 : numel(cnn.layers{2}.a)
+    plot(cnn.layers{2}.a{i}(:,1)); 
+end
+hold off;
+figure;
+hold on;
+for i = 1 : numel(cnn.layers{4}.a)
+    plot(cnn.layers{4}.a{i}(:,1)); 
+end
+hold off;
+
+% 6 x 2 x 13 x 2, epoch=1, error = 0.0116
+% 
+
+assert(er<0.12, 'Too big error');
